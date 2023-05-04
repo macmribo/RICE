db360e82aa50234cd986c5b9c2dda88f6087670a
Mac M Ribo <98993577+macmribo@users.noreply.github.com>
2023-05-04 17:44:10 -0600

    streamlined the file generation

---
+++ 3_RELOG_simulation/simulations/Recycling_cap_NAICS_5/solver.jl
@@ -0,0 +1,41 @@
+# Improve output style on Jupyter Notebook
+using Logging
+global_logger(ConsoleLogger(stdout));
+
+# Import package
+using RELOG, Gurobi, JuMP
+gurobi = optimizer_with_attributes(
+    Gurobi.Optimizer,
+    "TimeLimit" => 3600,
+    "MIPGap" => 0.001,
+)
+# Package to time cells
+using TickTock
+
+tick()
+# Solve optimization problem
+solution = RELOG.solve("/projects/rice/Recycling_cap_NAICS_5/input/case.json", optimizer=gurobi)
+
+# Write CSV report showing plant costs, capacities, energy expenditure and
+# utilization factors
+
+RELOG.write_plants_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/plants.csv")
+
+# Write CSV report showing amount of product sent from initial locations to plants,
+# and from one plant to another. Includes the distance between each pair of
+# locations, amount-distance shipped, transportation costs and energy expenditure
+
+RELOG.write_transportation_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/transportation.csv")
+
+# Write CSV report showing primary product amounts, locations and marginal costs
+
+RELOG.write_products_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/products.csv")
+
+RELOG.write_plant_emissions_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/plant_emissions.csv")
+
+RELOG.write_plant_outputs_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/plant_oputputs.csv")
+
+RELOG.write_transportation_emissions_report(solution, "/projects/rice/Recycling_cap_NAICS_5/output/transportation_emissions.csv")
+tock()
+
+
